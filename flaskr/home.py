49d8b97faf4bf8c13b139560634b7a6d66ad96a4
Leundai <lgalin6@illinois.edu>
2020-09-19 11:31:47 -0500

    Login and Register Functionality

--- flaskr/home.py
+++ flaskr/home.py
@@ -12,7 +12,7 @@ bp = Blueprint('home', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, author_id, email'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
